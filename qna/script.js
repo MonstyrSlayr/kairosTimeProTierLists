3fb827c891350119f6591e7f11c0e5b9f0894147
MonstyrSlayr <eehikioya0@gmail.com>
2026-05-12 22:33:54 -0400

    v51

--- qna/script.js
+++ qna/script.js
@@ -1,3 +1,13 @@
-import { replaceFooter } from "./data/data.js";
+import { getAllBrawlers, getAllTierLists, replaceFooter } from "../data/data.js";
 
 replaceFooter();
+
+const brawlers = await getAllBrawlers();
+
+const brawlersWithBuffies = brawlers.filter(b => b.buffies != null);
+
+const tierLists = await getAllTierLists();
+
+document.getElementById("version").textContent = tierLists[tierLists.length - 1].version;
+document.getElementById("buffieCount").textContent = brawlersWithBuffies.length;
+document.getElementById("totalCount").textContent = brawlers.length;
